5db7e2e478d519fdb691ec1f03c63115a18afdce
Vincent James T. Valero <vjtvalero@gmail.com>
2019-04-05 22:49:52 +0800

    get latest

--- src/manga/manga.controller.ts
+++ src/manga/manga.controller.ts
@@ -4,13 +4,14 @@ import * as cheerio from 'cheerio';
 
 @Controller('manga')
 export class MangaController {
-    baseUrl = 'https://readms.net/manga';
+    baseUrl = 'https://readms.net';
+    baseMangaUrl = 'https://readms.net/manga';
 
     @Get('/mangas')
     async getMangas() {
         let promise = new Promise((resolve, reject) => {
             let arr = [];
-            request(this.baseUrl, (error, response, html) => {
+            request(this.baseMangaUrl, (error, response, html) => {
                 if (!error && response.statusCode == 200) {
                     const $ = cheerio.load(html);
                     const mangas = $('.table.table-striped tbody').children('tr');
@@ -21,7 +22,40 @@ export class MangaController {
                         const mangaUrl = anchor.attr('href');
                         const mangaName = anchor.text().replace(/\s\s+/g, '');
                         if (mangaUrl) {
-                            arr.push({ url: this.baseUrl + mangaUrl, name: mangaName });
+                            arr.push({ url: this.baseMangaUrl + mangaUrl, name: mangaName });
+                        }
+                    });
+
+                    resolve(arr);
+                } else {
+                    reject(arr);
+                }
+            });
+        });
+
+        let result = await promise;
+        return result;
+    }
+
+    @Get('/latest')
+    async getLatest() {
+        let promise = new Promise((resolve, reject) => {
+            let arr = [];
+            request(this.baseMangaUrl, (error, response, html) => {
+                if (!error && response.statusCode == 200) {
+                    const $ = cheerio.load(html);
+                    const mangas = $('.new-list').eq(0).children('li');
+
+                    mangas.each((i, e) => {
+                        const manga = $(e);
+                        const anchor = manga.find('a');
+                        const mangaUrl = anchor.attr('href');
+                        const mangaName = anchor.clone().children().remove().end().text().trim().replace(/\s\s+/g, '');
+                        const chapterNumber = anchor.find('strong').text().replace(/\s\s+/g, '');
+                        const chapterName = anchor.find('em').text().replace(/\s\s+/g, '');
+                        const released = anchor.children().eq(0).text().replace(/\s\s+/g, '');
+                        if (mangaUrl) {
+                            arr.push({ url: this.baseUrl + mangaUrl, mangaName, chapterNumber, chapterName, released });
                         }
                     });
 
@@ -60,7 +94,7 @@ export class MangaController {
     async getChapters(@Param('url') url: string) {
         let promise = new Promise((resolve, reject) => {
             let arr = [];
-            let newUrl = this.baseUrl + '/' + url;
+            let newUrl = this.baseMangaUrl + '/' + url;
             request(newUrl, (error, response, html) => {
                 if (!error && response.statusCode == 200) {
                     const $ = cheerio.load(html);
